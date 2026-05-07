bca27f53d408fcb28559982dc54777f173eb0b49
Denis Cazzulo <denis.cazzulo@tesisquare.com>
2026-05-07 12:58:06 +0200

    riproviamo

--- core/components/ui/sheet.tsx
+++ core/components/ui/sheet.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { X } from "lucide-react";
 import { cn } from "@core/lib/utils";
 
@@ -42,9 +41,7 @@ const SheetContent = React.forwardRef<
       )}
       {...props}
     >
-      <VisuallyHidden>
-        <DialogPrimitive.Title>Menu di navigazione</DialogPrimitive.Title>
-      </VisuallyHidden>
+      <DialogPrimitive.Title className="sr-only">Menu di navigazione</DialogPrimitive.Title>
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 hover:opacity-100">
         <X className="h-5 w-5" />
